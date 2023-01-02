85543b6324263ff0ea6b7c57d28c4673eb0d1b9a
George Davidson <gd240673@gmail.com>
2023-01-02 17:39:00 -0500

    Added Commands
    Added 'stop' and 'help' commands, also changed where you put the chrome driver.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
+import os
 import threading
+import time
 
 from selenium import webdriver
 from selenium.webdriver import ActionChains
@@ -7,7 +8,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 # The location of your driver.
-PATH = "C:\Program Files (x86)\chromedriver.exe"
+PATH = os.path.join("driver", "chromedriver.exe")
 
 service = Service(executable_path=PATH)
 driver = webdriver.Chrome(service=service)
@@ -15,41 +16,67 @@ driver = webdriver.Chrome(service=service)
 driver.get("https://monkeytype.com")  # Gets the website.
 driver.implicitly_wait(30)  # Waiting for the website to fully load.
 
-delay = 0.01  # The time between each letter typed.
+
+class Main:
+    def __init__(self):
+        self.typing = False
+        self.delay = 0.01  # The time between each letter typed.
+
+    def typer(self):
+        while True:
+            if self.typing and (len(driver.find_element(By.ID, "words").find_element(By.CLASS_NAME, "word").text) != 0):
+                word = [letter for letter in driver.find_element(
+                    By.CSS_SELECTOR, ".word.active").text] + [" "]
+
+                for letter in word:
+                    # Sends the letter to the browser.
+                    ActionChains(driver).send_keys(letter).perform()
+            else:
+                self.typing = False
+
+            time.sleep(self.delay)
+
+    def main_loop(self):
+        while True:
+            command = input("> ").lower()
+
+            if command == "start":
+                if len(driver.find_element(By.ID, "words").text) != 0:
+                    self.typing = True
+                    print("Started Program...")
+                else:
+                    print("Failed to start.")
+
+            elif command == "stop":
+                self.typing = False
+                print("Program Stoped.")
+
+            elif command == "quit":
+                print("Quitting Program...")
+                driver.quit()
+                print("Almost Done...")
+                break
+
+            elif command == "help":
+                print("""'start' to start typing.
+'stop' to stop typing.
+'quit' to quit the program.""")
+            else:
+                print(
+                    f"'{command}' is not recognized. Type 'help' to see commands.")
+
+        print("Done")
 
 
-def letter_by_letter(delay):
-    while (len(driver.find_element(By.ID, "words").find_element(By.CLASS_NAME, "word").text) != 0):
-        word = [letter for letter in driver.find_element(
-            By.CSS_SELECTOR, ".word.active").text] + [" "]
+def main():
+    main = Main()
 
-        for letter in word:
-            # Sends the letter to the browser.
-            ActionChains(driver).send_keys(letter).perform()
-            time.sleep(delay)
+    typer_thred = threading.Thread(target=main.typer)
+    typer_thred.daemon = True
+    typer_thred.start()
 
-    print("Program Stopped.")
+    main.main_loop()
 
 
-while True:
-    command = input("> ").lower()
-
-    if command == "start":
-        if len(driver.find_element(By.ID, "words").text) != 0:
-            print("Started Program...")
-            letter_by_letter(delay)
-        else:
-            print("Failed to start.")
-
-    elif command == "quit":
-        print("Quitting Program...")
-        driver.quit()
-        print("Almost Done...")
-        break
-
-    else:
-        print(
-            f"'{command}' is not recognized. Type 'start' to start typing or 'quit' to quit the program."
-        )
-
-print("Done")
+if __name__ == "__main__":
+    main()
